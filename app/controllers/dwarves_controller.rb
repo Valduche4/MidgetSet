6dfea09c98f77482a025b7a02b9e3fdc7a2cd53e
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-05-23 17:17:23 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/dwarves_controller.rb
+++ app/controllers/dwarves_controller.rb
@@ -1,2 +1,54 @@
 class DwarvesController < ApplicationController
+  before_action :set_dwarf, only: [:show, :edit, :update, :destroy]
+  def index
+    @dwarves = Dwarf.all
+  end
+
+  def show
+  end
+
+  def new
+    @dwarf = Dwarf.new
+  end
+
+  def create
+    @dwarf = Dwarf.new(dwarf_params)
+    @dwarf.user = current_user
+    if @dwarf.save
+      redirect_to dwarves_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @dwarf = Dwarf.new(dwarf_params)
+    @dwarf.update
+    if @dwarf.save
+      redirect_to dwarf_path(@dwarf)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dwarf.destroy
+
+    redirect_to dwarves_path
+  end
+
+
+
+  private
+
+  def dwarf_params
+    params.require(:dwarf).permit(:name, :sexe, :origin, :age, :size, :description, :price, :city)
+  end
+
+  def set_dwarf
+    @dwarf = Dwarf.find(params[:id])
+  end
 end
--- app/models/dwarf.rb
+++ app/models/dwarf.rb
@@ -1,3 +1,12 @@
 class Dwarf < ApplicationRecord
   belongs_to :user
+  SEXES = %w[male female no-binary]
+  validates :name, presence: true
+  validates :sexe, presence: true, inclusion: { in: SEXES }
+  validates :origin, presence: true
+  validates :age, presence: true
+  validates :size, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :city, presence: true
 end
