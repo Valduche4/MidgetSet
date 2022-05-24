d7096986bd3c8fbd3a7e06801041ba34fbdace2b
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-05-24 12:07:33 +0200

    Basics of controllers reviews and reservations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @dwarf = Dwarf.find(params[:dwarf_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    @dwarf = Dwarf.find(params[:dwarf_id])
+    @review.dwarf = @dwarf
+    @review.user = current_user
+    if @review.save
+      redirect_to dwarf_path(@dwarf)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to dwarf_path(@review.dwarf)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
