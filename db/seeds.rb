af5c5d7fd0a84de2cf19c0b22ea57bf31fc9612c
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-05-26 16:38:41 +0200

    pour le sit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "faker"
+file = URI.open("https://picsum.photos/400/600")
 jobs = Job.create([{ name: "Wrestler" }, { name: "Stripteaser"}, { name: "Cosplay"}, { name: "Darts"}, { name: "Canonman"}, { name: "Friend"}, { name: "Bachelor animator"}, { name: "Signer"}])
+
+# 20.times do
+#   dwarf = Dwarf.new(
+#     name: Faker::Name.name,
+#     sexe: "male",
+#     origin: Faker::WorldCup.team,
+#     age: rand(1..100),
+#     size: rand(1...150),
+#     description: Faker::Lorem.sentence,
+#     price: rand(1...200),
+#     city: Faker::WorldCup.city
+#   )
+#   dwarf.jobs.name = jobs.name.rand
+#   dwarf.photo.attach(io: file, filename: "valentine.png", content_type: "image/png")
+#   dwarf.save!
+# end
